6e7bbf98b54e86733824054372a6cd74866b4997
mattn <mattn.jp@gmail.com>
2011-01-19 21:01:10 +0900

    add some methods.

--- webkit.go
+++ webkit.go
@@ -17,6 +17,8 @@ static inline void free_string(char* s) { free(s); }
 
 static inline gchar* to_gcharptr(const char* s) { return (gchar*)s; }
 
+static inline char* to_charptr(const gchar* s) { return (char*)s; }
+
 static WebKitWebView* to_WebKitWebView(void* w) { return WEBKIT_WEB_VIEW(w); }
 
 static void* _webkit_web_view_new() {
@@ -27,6 +29,19 @@ import "C"
 import "gtk"
 import "glib"
 import "unsafe"
+
+func bool2gboolean(b bool) C.gboolean {
+	if b {
+		return C.gboolean(1)
+	}
+	return C.gboolean(0)
+}
+func gboolean2bool(b C.gboolean) bool {
+	if b != 0 {
+		return true
+	}
+	return false
+}
 
 type WebKitWebView struct {
 	gtk.GtkWidget
@@ -41,21 +56,47 @@ func (v *WebKitWebView) LoadUri(uri string) {
 	C.webkit_web_view_load_uri(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), C.to_gcharptr(ptr))
 }
 // TODO
-//WEBKIT_API G_CONST_RETURN gchar *webkit_web_view_get_title (WebKitWebView *web_view);
-//WEBKIT_API G_CONST_RETURN gchar *webkit_web_view_get_uri (WebKitWebView *web_view);
+func (v *WebKitWebView) GetTitle() string {
+	return C.GoString(C.to_charptr(C.webkit_web_view_get_title(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))))
+}
+func (v *WebKitWebView) GetUri() string {
+	return C.GoString(C.to_charptr(C.webkit_web_view_get_uri(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))))
+}
 //WEBKIT_API void webkit_web_view_set_maintains_back_forward_list (WebKitWebView *web_view, gboolean flag);
 //WEBKIT_API WebKitWebBackForwardList *webkit_web_view_get_back_forward_list (WebKitWebView *web_view);
 //WEBKIT_API gboolean webkit_web_view_go_to_back_forward_item (WebKitWebView *web_view, WebKitWebHistoryItem *item);
-//WEBKIT_API gboolean webkit_web_view_can_go_back (WebKitWebView *web_view);
-//WEBKIT_API gboolean webkit_web_view_can_go_back_or_forward (WebKitWebView *web_view, gint steps);
-//WEBKIT_API gboolean webkit_web_view_can_go_forward (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_go_back (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_go_back_or_forward (WebKitWebView *web_view, gint steps);
-//WEBKIT_API void webkit_web_view_go_forward (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_stop_loading (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_open (WebKitWebView *web_view, const gchar *uri);
-//WEBKIT_API void webkit_web_view_reload (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_reload_bypass_cache (WebKitWebView *web_view);
+func (v *WebKitWebView) CanGoBack() bool {
+	return gboolean2bool(C.webkit_web_view_can_go_back(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) CanGoBackOrForward(steps int) bool {
+	return gboolean2bool(C.webkit_web_view_can_go_back_or_forward(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), C.gint(steps)))
+}
+func (v *WebKitWebView) CanGoForward() bool {
+	return gboolean2bool(C.webkit_web_view_can_go_forward(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) GoBack() {
+	C.webkit_web_view_go_back(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) GoBackOrForward(steps int) {
+	C.webkit_web_view_go_back_or_forward(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), C.gint(steps))
+}
+func (v *WebKitWebView) GoForward() {
+	C.webkit_web_view_go_forward(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) StopLoading() {
+	C.webkit_web_view_stop_loading(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) Open(uri string) {
+	ptr := C.CString(uri)
+	defer C.free_string(ptr)
+	C.webkit_web_view_open(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), C.to_gcharptr(ptr))
+}
+func (v *WebKitWebView) Reload() {
+	C.webkit_web_view_reload(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) ReloadBypassCache() {
+	C.webkit_web_view_reload_bypass_cache(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
 func (v *WebKitWebView) LoadString(content, mime_type, encoding, base_uri string) {
 	pcontent := C.CString(content)
 	defer C.free_string(pcontent)
@@ -67,7 +108,6 @@ func (v *WebKitWebView) LoadString(content, mime_type, encoding, base_uri string
 	defer C.free_string(pbase_uri)
 	C.webkit_web_view_load_string(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), C.to_gcharptr(pcontent), C.to_gcharptr(pmime_type), C.to_gcharptr(pencoding), C.to_gcharptr(pbase_uri))
 }
-//WEBKIT_API void webkit_web_view_load_html_string (WebKitWebView *web_view,  const gchar *content,  const gchar *base_uri);
 func (v *WebKitWebView) LoadHtmlString(content, base_uri string) {
 	pcontent := C.CString(content)
 	defer C.free_string(pcontent)
@@ -78,22 +118,52 @@ func (v *WebKitWebView) LoadHtmlString(content, base_uri string) {
 //WEBKIT_API void webkit_web_view_load_request (WebKitWebView *web_view, WebKitNetworkRequest *request);
 //WEBKIT_API gboolean webkit_web_view_search_text (WebKitWebView *web_view, const gchar *text, gboolean case_sensitive, gboolean forward, gboolean wrap);
 //WEBKIT_API guint webkit_web_view_mark_text_matches (WebKitWebView *web_view, const gchar *string, gboolean case_sensitive, guint limit);
-//WEBKIT_API void webkit_web_view_set_highlight_text_matches (WebKitWebView *web_view, gboolean highlight);
-//WEBKIT_API void webkit_web_view_unmark_text_matches (WebKitWebView *web_view);
+func (v *WebKitWebView) SetHighlightTextMatches(highlight bool) {
+	C.webkit_web_view_set_highlight_text_matches(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), bool2gboolean(highlight))
+}
+func (v *WebKitWebView) UnmarkTextMatches() {
+	C.webkit_web_view_unmark_text_matches(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
 //WEBKIT_API WebKitWebFrame * webkit_web_view_get_main_frame (WebKitWebView *web_view);
 //WEBKIT_API WebKitWebFrame * webkit_web_view_get_focused_frame (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_execute_script (WebKitWebView *web_view, const gchar *script);
-//WEBKIT_API gboolean webkit_web_view_can_cut_clipboard (WebKitWebView *web_view);
-//WEBKIT_API gboolean webkit_web_view_can_copy_clipboard (WebKitWebView *web_view);
-//WEBKIT_API gboolean webkit_web_view_can_paste_clipboard (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_cut_clipboard (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_copy_clipboard (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_paste_clipboard (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_delete_selection (WebKitWebView *web_view);
-//WEBKIT_API gboolean webkit_web_view_has_selection (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_select_all (WebKitWebView *web_view);
-//WEBKIT_API gboolean webkit_web_view_get_editable (WebKitWebView *web_view);
-//WEBKIT_API void webkit_web_view_set_editable (WebKitWebView *web_view, gboolean flag);
+func (v *WebKitWebView) ExecuteScript(script string) {
+	pscript := C.CString(script)
+	defer C.free_string(pscript)
+	C.webkit_web_view_execute_script(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), C.to_gcharptr(pscript))
+}
+func (v *WebKitWebView) CanCutClipboard() bool {
+	return gboolean2bool(C.webkit_web_view_can_cut_clipboard(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) CanCopyClipboard() bool {
+	return gboolean2bool(C.webkit_web_view_can_copy_clipboard(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) CanPasteCilpboard() bool {
+	return gboolean2bool(C.webkit_web_view_can_paste_clipboard(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) CutClipboard() {
+	C.webkit_web_view_cut_clipboard(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) CopyClipboard() {
+	C.webkit_web_view_copy_clipboard(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) PasteCilpboard() {
+	C.webkit_web_view_paste_clipboard(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) DeleteSelection() {
+	C.webkit_web_view_delete_selection(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) HasSelection() bool {
+	return gboolean2bool(C.webkit_web_view_has_selection(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) SelectAll() {
+	C.webkit_web_view_select_all(C.to_WebKitWebView(unsafe.Pointer(v.Widget)))
+}
+func (v *WebKitWebView) GetEditable() bool {
+	return gboolean2bool(C.webkit_web_view_get_editable(C.to_WebKitWebView(unsafe.Pointer(v.Widget))))
+}
+func (v *WebKitWebView) SetEditable(flag bool) {
+	C.webkit_web_view_set_editable(C.to_WebKitWebView(unsafe.Pointer(v.Widget)), bool2gboolean(flag))
+}
 //WEBKIT_API GtkTargetList * webkit_web_view_get_copy_target_list (WebKitWebView *web_view);
 //WEBKIT_API GtkTargetList * webkit_web_view_get_paste_target_list (WebKitWebView *web_view);
 //WEBKIT_API void webkit_web_view_set_settings (WebKitWebView *web_view, WebKitWebSettings *settings);
