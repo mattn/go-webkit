31e3fea1b1b3a1fbe17d9e007139affb39a3dc3b
mattn <mattn.jp@gmail.com>
2011-01-20 17:45:03 +0900

    move html source to head.

--- example/webview.go
+++ example/webview.go
@@ -4,6 +4,25 @@ import "os"
 import "gtk"
 import "webkit"
 
+const HTML_STRING = `
+<doctype html>
+<meta charset="utf-8"/>
+<style>
+div { font-size: 5em }
+</style>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+$(function() {
+    $('#hello1').slideDown('slow', function() {
+    	$('#hello2').fadeIn()
+	})
+})
+</script>
+<div id="hello1" style="display: none">Hello</div>
+<div id="hello2" style="display: none">世界</div>
+</div>
+`
+
 func main() {
 	gtk.Init(nil)
 	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
@@ -23,41 +42,28 @@ func main() {
 	webview := webkit.WebView()
 	webview.Connect("load-committed", func() {
 		entry.SetText(webview.GetUri())
-	}, nil)
+	},
+		nil)
 	swin.Add(webview)
 
 	vbox.Add(swin)
 
 	entry.Connect("activate", func() {
 		webview.LoadUri(entry.GetText())
-	}, nil)
+	},
+		nil)
 	button := gtk.ButtonWithLabel("load String")
 	button.Clicked(func() {
 		webview.LoadString("hello Go GTK!", "text/plain", "utf-8", ".")
-	}, nil)
+	},
+		nil)
 	vbox.PackStart(button, false, false, 0)
 
 	button = gtk.ButtonWithLabel("load HTML String")
 	button.Clicked(func() {
-		webview.LoadHtmlString(`
-<doctype html>
-<meta charset="utf-8"/>
-<style>
-div { font-size: 5em }
-</style>
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script>
-$(function() {
-    $('#hello1').slideDown('slow', function() {
-    	$('#hello2').fadeIn()
-	})
-})
-</script>
-<div id="hello1" style="display: none">Hello</div>
-<div id="hello2" style="display: none">世界</div>
-</div>
-`, ".")
-	}, nil)
+		webview.LoadHtmlString(HTML_STRING, ".")
+	},
+		nil)
 	vbox.PackStart(button, false, false, 0)
 
 	window.Add(vbox)
